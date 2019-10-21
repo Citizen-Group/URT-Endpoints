0ac260ae7ce0b81baa9a7ca44de18577d4d4bebf
Justin Kelly <Blue_Lt@hotmail.com>
2019-10-21 12:57:22 -0400

    changes

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-// Node Endpoint Test App
+/**
+ * Universal Reporting Tool: Endpoint Template 
+ * 
+ * This project provides the template and validation for creating a URT Endpoint.
+ * 
+ * See: https://github.com/Citizen-Group/URT-Endpoints
+ */
 var fs = require('fs');
+
+// Include model files
+fs.readdirSync(__dirname + '/').forEach(function(filename) {
+    if (~filename.indexOf('.js')) {
+       console.log(__dirname + '/' + filename);
+    } 
+});
